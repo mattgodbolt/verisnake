1e9f92d89d8bd56133853f5bc824b14cb428e07b
Matt Godbolt <matt@godbolt.org>
2023-01-16 20:45:52 -0600

    work in progress but...delayed

---
+++ play_area.v
@@ -0,0 +1,24 @@
+`default_nettype none
+
+module play_area (
+    input wire clk,
+    input wire reset,
+    input wire [6:0] x,  // or whatever
+    input wire [5:0] y,  // or whatever
+    input wire write_enable,
+    input wire write_value,
+    output reg out
+);
+  reg RAM[(1<<11)-1:0];
+  always @(posedge clk) begin
+    if (reset) begin
+      out <= 0;
+    end else begin
+      if (write_enable) begin
+        RAM[{y, x}] <= write_value;
+      end
+      out <= RAM[{y, x}];
+    end
+  end
+
+endmodule
--- snake.v
+++ snake.v
@@ -21,6 +21,19 @@ module snake (
   reg  [6:0] game_speed;
   reg        prev_vsync;
 
+  wire       ram_out;
+  reg        ram_in = 1;
+  reg        ram_value = 0;
+  play_area play_area (
+      .clk(clk),
+      .reset(reset),
+      .x(pos_x[9:3]),
+      .y(pos_y[8:3]),
+      .write_enable(ram_in),
+      .write_value(ram_value),
+      .out(ram_out)
+  );
+
   vga vga (
       .clk(clk),
       .reset(reset),
@@ -39,12 +52,18 @@ module snake (
       food_x <= 7'd20;
       food_y <= 6'd30;
       game_speed = 7'd60;
+      // clear game area:
+      ram_in <= 1;
+      ram_value <= 0;
     end else begin
+      ram_value  <= pos_x[3] ^ pos_y[3];
       prev_vsync <= vsync;
       if (!prev_vsync && vsync) begin
         if (divider == game_speed) begin
           led = !led;
           divider <= 0;
+          // poor man's state machine, stop clearing area
+          ram_in  <= 0;
         end else begin
           divider <= divider + 1;
         end
@@ -56,10 +75,11 @@ module snake (
   wire [5:0] y_block = pos_y[8:3];
   wire on_food = x_block == food_x && y_block == food_y;
   wire on_play_edge = pos_x <= 2 || pos_x >= 637 || pos_y <= 2 || pos_y >= 477;
+  wire on_snake = ram_out;
 
   assign led_centre = led;
   assign red = display_on && on_food;
   assign green = display_on && on_food;
-  assign blue = display_on && on_play_edge;
+  assign blue = display_on && (on_play_edge || on_snake);
 
 endmodule
